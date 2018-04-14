356324ed3098466d90925b7480a7422535f31411
Clay McCoy <clay@atomist.com>
2018-04-13 21:55:05 -0500

    initial Spring Boot Project from spring initializer

---
+++ src/main/kotlin/com/atomist/rolar/RolarApplication.kt
@@ -0,0 +1,11 @@
+package com.atomist.rolar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RolarApplication
+
+fun main(args: Array<String>) {
+    runApplication<RolarApplication>(*args)
+}
