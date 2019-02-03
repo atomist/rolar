565bbaa8ab75bbefa44efd344663782602ad609b
Lieven Doclo <lieven@atomist.com>
2019-02-03 20:52:39 +0100

    Fixed configuration for cloudfoundry backwards compatibility

--- src/main/kotlin/com/atomist/rolar/infra/s3/S3LoggingServiceProperties.kt
+++ src/main/kotlin/com/atomist/rolar/infra/s3/S3LoggingServiceProperties.kt
@@ -1,10 +1,10 @@
 package com.atomist.rolar.infra.s3
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Component
 
-@Configuration
-@ConfigurationProperties("atomist.rolar")
+@Component
+@ConfigurationProperties("vcap.services.s3-logging.credentials")
 data class S3LoggingServiceProperties(var aws_access_key_id: String = "",
                                       var aws_secret_access_key: String = "",
                                       var s3_logging_bucket: String = "",
