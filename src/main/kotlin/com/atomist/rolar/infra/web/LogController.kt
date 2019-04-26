5c6b29e3ad313f230aef7fc952bfbd4d29788e19
Lieven Doclo <lieven@atomist.com>
2019-04-26 23:01:07 +0200

    Committing just the rate limiting code

--- src/main/kotlin/com/atomist/rolar/infra/web/LogController.kt
+++ src/main/kotlin/com/atomist/rolar/infra/web/LogController.kt
@@ -4,6 +4,7 @@ import com.atomist.rolar.app.*
 import com.atomist.rolar.domain.model.IncomingLog
 import com.atomist.rolar.domain.model.LogLine
 import com.atomist.rolar.domain.model.LogResults
+import io.github.resilience4j.ratelimiter.RateLimiterRegistry
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.HandlerMapping
@@ -15,52 +16,63 @@ import javax.servlet.http.HttpServletRequest
 
 @CrossOrigin
 @RestController
-class LogController @Autowired
-constructor(private var getLogs: GetLogs, private var streamLogs: StreamLogs, private var writeLog: WriteLog) {
+class LogController @Autowired constructor(
+        private var getLogs: GetLogs,
+        private var streamLogs: StreamLogs,
+        private var writeLog: WriteLog,
+        private val rateLimiterRegistry: RateLimiterRegistry) {
 
     @RequestMapping("api/logs/**")
     fun getLog(@RequestParam prioritize: Int? = 0,
                @RequestParam historyLimit: Int? = 0,
                request: HttpServletRequest): List<LogResults> {
-        val path = constructPathFromUriWildcardSuffix(request)
-        val consumer = SimpleStateConsumer<GetLogsResponse>()
-        getLogs.getLogs(GetLogsRequest(path, prioritize ?: 0, historyLimit ?: 0), consumer)
-        return consumer.value
+        return rateLimiterRegistry.rateLimiter("rolar").executeSupplier {
+            val path = constructPathFromUriWildcardSuffix(request)
+            val consumer = SimpleStateConsumer<GetLogsResponse>()
+            getLogs.getLogs(GetLogsRequest(path, prioritize ?: 0, historyLimit ?: 0), consumer)
+            consumer.value
+        }
     }
 
     @RequestMapping(path = ["api/logs/**"], method = [RequestMethod.HEAD])
     fun getLog(): Long {
-        val consumer = SimpleStateConsumer<WriteLogResponse>()
-        writeLog.writeLog(WriteLogRequest(listOf("service_testing"), true, IncomingLog(
-                "unknown",
-                listOf(LogLine(
-                        "info",
-                        "testing that service is up",
-                        Date().time.toString(),
-                        Date().time
-                ))
-        )), consumer)
-        return consumer.value
+        return rateLimiterRegistry.rateLimiter("rolar").executeSupplier {
+            val consumer = SimpleStateConsumer<WriteLogResponse>()
+            writeLog.writeLog(WriteLogRequest(listOf("service_testing"), true, IncomingLog(
+                    "unknown",
+                    listOf(LogLine(
+                            "info",
+                            "testing that service is up",
+                            Date().time.toString(),
+                            Date().time
+                    ))
+            )), consumer)
+            consumer.value
+        }
     }
 
     @GetMapping(value = ["api/reactive/logs/**"])
     fun getLogs(@RequestParam prioritize: Int? = 0,
                 @RequestParam historyLimit: Int? = 0,
                 request: HttpServletRequest): SseEmitter {
-        val path = constructPathFromUriWildcardSuffix(request)
-        val sseEmitter = SseEmitter()
-        streamLogs.getLogs(StreamLogsRequest(path, prioritize ?: 0, historyLimit ?: 0), SsePublisher(sseEmitter))
-        return sseEmitter
+        return rateLimiterRegistry.rateLimiter("rolar").executeSupplier {
+            val path = constructPathFromUriWildcardSuffix(request)
+            val sseEmitter = SseEmitter()
+            streamLogs.getLogs(StreamLogsRequest(path, prioritize ?: 0, historyLimit ?: 0), SsePublisher(sseEmitter))
+            sseEmitter
+        }
     }
 
     @RequestMapping(value = ["api/logs/**"], method = arrayOf(RequestMethod.POST))
     fun postLog(@RequestParam closed: Boolean? = false,
                 @RequestBody incomingLog: IncomingLog,
                 request: HttpServletRequest): Long {
-        val path = constructPathFromUriWildcardSuffix(request)
-        val consumer = SimpleStateConsumer<WriteLogResponse>()
-        writeLog.writeLog(WriteLogRequest(path, closed ?: false, incomingLog), consumer)
-        return consumer.value
+        return rateLimiterRegistry.rateLimiter("rolar").executeSupplier {
+            val path = constructPathFromUriWildcardSuffix(request)
+            val consumer = SimpleStateConsumer<WriteLogResponse>()
+            writeLog.writeLog(WriteLogRequest(path, closed ?: false, incomingLog), consumer)
+            consumer.value
+        }
     }
 
     private fun constructPathFromUriWildcardSuffix(request: HttpServletRequest): List<String> {
